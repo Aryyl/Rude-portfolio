bcc8b4e3592a4b272d362f49b2c34f050cd10e61
Aryyaman Bora <aryyamanboratskab04@gmail.com>
2026-08-17 23:01:04 +0530

    Convert Google Drive iframes to native video streams

--- src/components/CarouselGallery.tsx
+++ src/components/CarouselGallery.tsx
@@ -73,8 +73,8 @@ const PROJECTS: Project[] = [
     category: "Cinematic",
     year: "2026",
     role: "Editor",
-    type: "iframe",
-    src: "https://drive.google.com/file/d/1330uAktITPYX9zKMKQUHQX2ybHvk3C_A/preview",
+    type: "video",
+    src: "https://drive.google.com/uc?export=download&id=1330uAktITPYX9zKMKQUHQX2ybHvk3C_A",
     isVertical: true,
   },
   {
@@ -83,8 +83,8 @@ const PROJECTS: Project[] = [
     category: "Documentary",
     year: "2026",
     role: "Cinematographer",
-    type: "iframe",
-    src: "https://drive.google.com/file/d/13Tczh56kPa5Q-TMqZpmE7ry47ZZ64tTC/preview",
+    type: "video",
+    src: "https://drive.google.com/uc?export=download&id=13Tczh56kPa5Q-TMqZpmE7ry47ZZ64tTC",
     isVertical: true,
   },
   {
@@ -93,8 +93,8 @@ const PROJECTS: Project[] = [
     category: "Experimental",
     year: "2026",
     role: "Director",
-    type: "iframe",
-    src: "https://drive.google.com/file/d/1wXkAJEh_KvW6nJOTKdp43pxbjEvm3GPW/preview",
+    type: "video",
+    src: "https://drive.google.com/uc?export=download&id=1wXkAJEh_KvW6nJOTKdp43pxbjEvm3GPW",
     isVertical: true,
   }
 ];
